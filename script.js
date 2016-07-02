c34b5725e16a8d0b6f43efe7241f17ffd3f579ca
Gianteraser <itsdavidthai@gmail.com>
2016-07-02 02:00:14 -0700

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-
-function setAuth(){
+// put the oauth code into a function that can be called
+// #zipCode.val() will be saved when the page loads and we have entered something
+// in the box
+function loadPage(){
     function cb(data) {        
             console.log("cb: " + JSON.stringify(data));
-}
+    }
 
     var auth = {
         //
@@ -46,100 +48,136 @@ function setAuth(){
     OAuth.SignatureMethod.sign(message, accessor);
 
     var parameterMap = OAuth.getParameterMap(message.parameters);
-
+    // TODO we are gonna want to use grabYelpDataV2 instead of this,
+    // this is low priority though it works for now
     grabYelpData();
-// when randomize button is clicked
 
-function onClickRandomize(content){
-        $('#randomize').click(function(){
-        $('.thumbnail').empty();
-        grabYelpDataV2(function(content){
-            var data = content
-            appendInfoToElement(data, 'one')
+    // not working yet but it's supposed to randomize when you hit enter in the textbox
+    function onEnterKeyRandomizer(content){
+            $('#randomize').bind("enterKey", function(e){
+               $('.thumbnail').empty();
+                grabYelpDataV2(function(content){
+                    var data = content
+                    appendInfoToElement(data, 'one')
+
+                })
+
+            })
+
+            $('#randomize').keyup(function(e){
+                if(e.keyCode == 13){
+                    $(this).trigger("enterKey");
+                }
+
+            })    
+    }
+
+    /*
+    assign or randomize button to send a ajax call every time it is clicked
+    */
+    function onClickRandomize(content){
+
+
+            $('#randomize').click(function(){
+            $('.thumbnail').empty();
+            grabYelpDataV2(function(content){
+                var data = content
+                appendInfoToElement(data, 'one')
+
+
+            })
 
         })
 
-    })
-
-}
+    }
 
 
-function grabYelpData(){
-        
-        $.ajax({
-        'url' : message.action,
-        'data' : parameterMap,
-        'dataType' : 'jsonp',
-        'jsonpCallback' : 'cb',
-        'cache': true
-    })
-    .success(
 
-        (function(content){
-            var data = content
-          appendInfoToElement(data, 'one');
-          onClickRandomize(data);
-          // appendInfoToElement(data, 'two');
-          // appendInfoToElement(data, 'three');
-          // appendInfoToElement(data, 'four');
-          // appendInfoToElement(data, 'five');
-          // appendInfoToElement(data, 'six');
-/*          $('#randomize').click(function(){
-            $('.thumbnail').empty();
-            appendInfoToElement(data, 'one');
-            // appendInfoToElement(data, 'two');
-            // appendInfoToElement(data, 'three');
-            // appendInfoToElement(data, 'four');
-            // appendInfoToElement(data, 'five');
-            // appendInfoToElement(data, 'six');
-              return false;
-          })
-*/        })
-    )
-    .done(function(data, textStatus, jqXHR) {
-            console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-        }
-    )
-    .fail(function(jqXHR, textStatus, errorThrown) {
-                        console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+    // intial grab of data 
+    //TODO: the intial load relies on this, refactor the code so we 
+    // we only have to rely on grabYelpDataV2
+    function grabYelpData(){
+            
+            $.ajax({
+            'url' : message.action,
+            'data' : parameterMap,
+            'dataType' : 'jsonp',
+            'jsonpCallback' : 'cb',
+            'cache': true
+        })
+        .success(
+
+            (function(content){
+                var data = content
+              appendInfoToElement(data, 'one');
+              onEnterKeyRandomizer(data);
+              onClickRandomize(data);
+
+              // appendInfoToElement(data, 'two');
+              // appendInfoToElement(data, 'three');
+              // appendInfoToElement(data, 'four');
+              // appendInfoToElement(data, 'five');
+              // appendInfoToElement(data, 'six');
+    /*          $('#randomize').click(function(){
+                $('.thumbnail').empty();
+                appendInfoToElement(data, 'one');
+                // appendInfoToElement(data, 'two');
+                // appendInfoToElement(data, 'three');
+                // appendInfoToElement(data, 'four');
+                // appendInfoToElement(data, 'five');
+                // appendInfoToElement(data, 'six');
+                  return false;
+              })
+    */        })
+        )
+        .done(function(data, textStatus, jqXHR) {
+                console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
             }
-    );
+        )
+        .fail(function(jqXHR, textStatus, errorThrown) {
+                            console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+                }
+        );
 
 
-}
-        
-function grabYelpDataV2(successCB){
-    setAuth();
-        $.ajax({
-        'url' : message.action,
-        'data' : parameterMap,
-        'dataType' : 'jsonp',
-        'jsonpCallback' : 'cb',
-        'cache': true
-    })
-    .success(
-        function(content){
-            successCB(content)
-        }
-        
-    )
-    .done(function(data, textStatus, jqXHR) {
-            console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-        }
-    )
-    .fail(function(jqXHR, textStatus, errorThrown) {
-                        console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+    }
+    /*
+    loads the page to grab the zip code value
+    takes a callback that success invokes 
+
+    */
+    function grabYelpDataV2(successCB){
+        loadPage();
+            $.ajax({
+            'url' : message.action,
+            'data' : parameterMap,
+            'dataType' : 'jsonp',
+            'jsonpCallback' : 'cb',
+            'cache': true
+        })
+        .success(
+            function(content){
+                successCB(content)
             }
-    );
+            
+        )
+        .done(function(data, textStatus, jqXHR) {
+                console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+            }
+        )
+        .fail(function(jqXHR, textStatus, errorThrown) {
+                            console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+                }
+        );
+
+
+    }
+
 
 
 }
 
 
 
-}
-
-
-
-
-setAuth()
+// ini/tal start of the webpage
+loadPage()
