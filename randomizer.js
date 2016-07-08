c904477899e5db966415fc08cbf817604d4474ba
Gianteraser <itsdavidthai@gmail.com>
2016-07-07 19:34:40 -0700

    refactored create element

--- randomizer.js
+++ randomizer.js
@@ -28,19 +28,7 @@ function appendInfoToElement(data){
     //this function randomly choose one set of data, and converts data to DOM
     sixRandomlySelectedRestaurantsArr = grab6Restaurants(data);
 
-    function createElement(data){
-        // var content = randomizer(data); 
-        var content = data; 
-        var name = '<h2>'+content.name+'</h2>';
-        var address = '',contentHTMl=';';
-        var link = '<p><button class="btn btn-default">'+'<a target="_blank" href="'+ content.url + '">Learn More</a>'+"</button></p>";
-        var review = '<img src="' + content.rating_img_url_large + '">'
-            content.location.display_address.forEach(function(add){
-                    address += '<p>' + add + '</p>'
-            });
-        var contentHTML = name + address + review + link;
-        return contentHTML  
-    }
+
     //append to page
     ids.forEach(function(val, index){
         $(val).append(createElement(sixRandomlySelectedRestaurantsArr[index]))
@@ -65,21 +53,23 @@ function mostReviewed(dataWeRetrievedFromYelpJSON) {
 function appendMostReivewedToElement(data) {
   $('.thumbnail').empty();
   var reviewed = mostReviewed(data);
-  function createElement(data) {
-    var content = data;
-    var name = '<h2>' + content.name + '</h2>';
-    var address = '',
-      contentHTMl = ';';
-    var link = '<p><button class="btn btn-default">' + '<a target="_blank" href="' + content.url + '">Learn More</a>' + "</button></p>";
-    var review = '<img src="' + content.rating_img_url_large + '">'
-    content.location.display_address.forEach(function(add) {
-      address += '<p>' + add + '</p>'
-    });
-    var contentHTML = name + address + review + link;
-    return contentHTML
-  }
+
   //append to page
   ids.forEach(function(val, index) {
     $(val).append(createElement(reviewed[index]))
   })
+}
+// appends the information from the yelp object
+function createElement(data){
+
+    var content = data; 
+    var name = '<h2>'+content.name+'</h2>';
+    var address = '',contentHTMl=';';
+    var link = '<p><button class="btn btn-default">'+'<a target="_blank" href="'+ content.url + '">Learn More</a>'+"</button></p>";
+    var review = '<img src="' + content.rating_img_url_large + '">'
+        content.location.display_address.forEach(function(add){
+                address += '<p>' + add + '</p>'
+        });
+    var contentHTML = name + address + review + link;
+    return contentHTML  
 }
